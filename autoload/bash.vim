89950af574879b90f18ac1d2d37f5cdb0dab7bc4
prim <1210102@qq.com>
2021-08-04 17:23:09 +0800

    zz

--- autoload/bash.vim
+++ autoload/bash.vim
@@ -3,3 +3,27 @@ let s:pluginPath = expand('<sfile>:p:h') . '/..'
 function! bash#complete(partialCommand)
     return systemlist(s:pluginPath . '/completer.sh ' . a:partialCommand)
 endfunction
+
+function! bash#completor(findstart, base)
+    if a:findstart
+        " locate the start of the word
+        let s:line = getline('.')
+        let line = getline('.')
+        let start = col('.') - 1
+        " echom 'init start' start
+        while start > 0 && line[start] != ' '
+            let start -= 1
+            " echom 'dec start' start
+        endwhile
+        let start += 1
+        " echom 'return start' start
+        return start
+    else
+        let ret = systemlist(s:pluginPath . '/completer.sh ' . s:line)
+        " echom 'base' a:base s:line ret
+        return ret
+    endif
+
+endfunction
+
+" set completefunc=bash#completor
--- completer.sh
+++ completer.sh
@@ -1,19 +1,24 @@
-#! /usr/bin/env bash
+#!/bin/bash
 
 # Adapted from: Brian Beffa <brbsix@gmail.com>
 # Original version: https://brbsix.github.io/2015/11/29/accessing-tab-completion-programmatically-in-bash/
 
 # load bash-completion if necessary
-completionDir="${BASH_COMPLETION_DIR:-/usr/share/bash-completion}"
 declare -F _completion_loader &>/dev/null || {
-    source "${completionDir}/bash_completion"
+    source /usr/share/bash-completion/bash_completion
 }
 
 COMP_TYPE=9 # <tab> pressed (63 would be <tab><tab>)
 COMP_KEY=9 # <tab>
 COMP_LINE=$*
 COMP_POINT=${#COMP_LINE}
+
+# echo '$@' $@ $#
 COMP_WORDS=("$@")
+# echo COMP_WORDS ${COMP_WORDS}
+
+eval set -- "$@"
+
 # index of the last word
 COMP_CWORD=$(( ${#COMP_WORDS[@]} - 1 ))
 
@@ -29,6 +34,9 @@ if [[ -z ${completion} ]]; then
     completion=$(complete -p "$1" 2>/dev/null | awk '{print $(NF-1)}')
 fi
 
+# complete -p | ag kube
+# echo $completion
+
 # ensure completion was detected
 if [[ -z ${completion} ]]; then
     exit 127
@@ -39,8 +47,19 @@ if [[ ${#COMP_WORDS[@]} -gt 1 ]]; then
     wordBeforeCompletion=${COMP_WORDS[-2]}
 fi
 
+# echo COMP_LINE ${COMP_LINE}
+# echo COMP_POINT ${COMP_POINT}
+# echo COMP_WORDS ${COMP_WORDS}
+# echo COMP_CWORD ${COMP_CWORD}
+
 # execute completion function
 "$completion" "${COMP_WORDS[0]}" "${COMP_WORDS[-1]}" "${wordBeforeCompletion}" 2>/dev/null
+
+# echo COMP_LINE ${COMP_LINE}
+# echo COMP_POINT ${COMP_POINT}
+# echo COMP_WORDS ${COMP_WORDS}
+# echo COMP_CWORD ${COMP_CWORD}
+# echo COMPREPLY ${COMPREPLY}
 
 # print completions
 printf '%s\n' "${COMPREPLY[@]}"
